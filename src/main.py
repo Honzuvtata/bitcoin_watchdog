b137f3e2a01966a008f9f1e285b5b45d5e97dbf0
Jan Svehla <jsvehla@amberg.cz>
2024-10-27 19:58:37 +0100

    add main.py

---
+++ src/main.py
@@ -0,0 +1,18 @@
+import requests
+
+
+def main():
+    pass
+#send request to get bitcoin current price
+def get_bitcoin_price():
+    pass
+
+requests.get
+
+#send email with bitcooin price 
+def send_bitcoin_info_email():
+    pass
+
+
+if self.__name__ == "__main__":
+    main()
